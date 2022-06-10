0ba408b485ee8ae7dc80433d0f7e47466d49cb12
Yorkin1998 <abdukhkhorov@mail.ru>
2022-06-10 15:59:40 +0500

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
 # if [ "$DATABASE" = "postgres" ]
